ba87ab58d1b97164fb1e115271b21beb60ae86be
fakhrifawwaz <fakhri07fawwaz@gmail.com>
2026-09-07 09:43:58 +0700

    Modifikasi enkapsulasi

--- Jobsheet3/Motor.java
+++ Jobsheet3/Motor.java
@@ -26,8 +26,12 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-        if (!this.statusMesin && kecepatan > 0) {
+        if (kecepatan < 0) {
+            System.out.println("Kecepatan tidak boleh bernilai negatif");
+        } else if (!this.statusMesin && kecepatan > 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100){
+            System.out.println("Kecepatan tidak boleh lebih dari 100");
         } else {
             this.kecepatan = kecepatan;
         }
