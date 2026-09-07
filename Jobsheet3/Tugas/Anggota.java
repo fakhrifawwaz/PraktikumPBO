07abc3d00b4a90862f0e2772b17c0f16642bc157
fakhrifawwaz <fakhri07fawwaz@gmail.com>
2026-09-07 12:31:21 +0700

    Tugas

---
+++ Jobsheet3/Tugas/Anggota.java
@@ -0,0 +1,42 @@
+package Jobsheet3.Tugas;
+
+public class Anggota {
+
+    private String noKTP;
+    private String nama;
+    private double limitPinjaman;
+    private double jumlahPinjaman;
+
+    public Anggota(String noKTP, String nama, double limitPinjaman) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getnama() {
+        return nama;
+    }
+    public double getlimitPinjaman() {
+        return limitPinjaman;
+    }
+    public double getjumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(double nominal) {
+        if (jumlahPinjaman + nominal > limitPinjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += nominal;
+        }
+    }
+
+    public void angsur(double nominal) {
+        if (nominal < 0.1 * jumlahPinjaman) {
+            System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman.");
+        } else {
+            jumlahPinjaman -= nominal;
+        }
+    }
+}
